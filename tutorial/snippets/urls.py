c11a479eb1b5ba62f2a2948232367e8473d6d4fe
David Zhang <cajan2@163.com>
2017-05-25 20:04:36 +0800

    tutorial 2 completed

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -5,11 +5,13 @@
 @author: david
 @time: 5/25/17 5:44 PM
 """
-
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
